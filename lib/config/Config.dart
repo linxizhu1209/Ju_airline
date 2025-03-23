b2ca6f498e3f4af69320daa4b99a369205e4dc19
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2025-03-23 15:15:26 +0900

    feat: config파일

---
+++ lib/config/Config.dart
@@ -0,0 +1,7 @@
+class Config {
+  static const String emulatorHost = "http://10.0.2.2:8081";
+  static const String realDeviceHost = "http://192.168.219.102:8081";
+
+  static const String baseUrl = realDeviceHost;
+
+}
